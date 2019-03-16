0d66eade3d465274e1876674bdaf1352da2c62e0
Benjamin Delmée <benjamin.delmee@gmail.com>
2019-03-16 12:52:53 +0100

    basic database connection

--- bdelmee/app/dataCollector/scripts/main.py
+++ bdelmee/app/dataCollector/scripts/main.py
@@ -1,1 +1,21 @@
-print("hello world!")
+import psycopg2
+import os
+
+if __name__ == '__main__':
+
+    conn_string = "host='{host}' dbname='{dbname}' user='{user}' password='{password}'".format(
+        host = os.getenv('DB_HOST', '127.0.0.1'),
+        dbname = 'bitmex',
+        user = 'user',
+        password = '3hGQv25CRdQQb8Cy'
+    )
+
+    host = os.getenv('DB_HOST', '127.0.0.1')
+
+    # get a connection, if a connect cannot be made an exception will be raised here
+    conn = psycopg2.connect(conn_string)
+
+    # conn.cursor will return a cursor object, you can use this cursor to perform queries
+    cursor = conn.cursor()
+
+    print("Connected!\n")
